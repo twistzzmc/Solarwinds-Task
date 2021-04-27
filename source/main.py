5aca046fc645da965cf4808fa7b16434899aec97
Michał <michal.szczepaniak1999@gmail.com>
2021-04-27 22:09:17 +0200

    added validation

--- source/main.py
+++ source/main.py
@@ -1,15 +1,40 @@
 import csv
+from datetime import datetime
 from source.Day import Day
 from source.Week import Week
+
+
+def validate_row(i, row, prev_date):
+    if row[1] not in {'Reader entry', 'Reader exit'}:
+        raise ValueError(
+            f'ERROR: [line {i + 1}] Events must be either \'Reader entry\' or \'Reader exit\', not: [{row[1]}]')
+
+    try:
+        previous_date = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S ')
+    except ValueError:
+        raise ValueError(f'ERROR [line {i + 1}] Bad date format: {row[0]}')
+
+    if i > 1 and \
+            datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S ') < prev_date:
+        raise ValueError(f'ERROR: [lines {i + 1}, {i}] Dates must in ascending order!')
+
+    return previous_date
 
 
 def get_days(file):
     days = []
     day_entries = []
 
+    previous_date = None
+
     for i, row in enumerate(file):
         if i == 0:
+            if row[0] != 'Date' or row[1] != 'Event' or row[2] != 'Gate' or len(row) != 3:
+                raise ValueError(f'ERROR: [line {i+1}] Bad columns, should be \'Date;Event;Gate\'')
+
             continue
+
+        previous_date = validate_row(i, row, previous_date)
 
         if len(day_entries) == 0 or row[0].split(' ')[0] == day_entries[len(day_entries) - 1][0].split(' ')[0]:
             day_entries.append(row)
